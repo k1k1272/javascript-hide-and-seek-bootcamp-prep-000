b48d8496e1a395fcded0198daaa67ce5555ae537
github username <github email address>
2018-01-31 21:49:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 function getFirstSelector(selector) {
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 }
 
 function nestedTarget() {
-  return document.getElementById("nested")
+  return document.getElementById("nested");
 }
 
 function increaseRankBy(n) {
-  const lis = document.querySelectorAll('ul.ranked-list li')
-  for (let i=0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  const rankedLists = document.querySelectorAll(".ranked-list");
+  for (var i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = rankedLists[i] += 1;
   }
 }
 
 function deepestChild() {
-  var answer
-  var children = {}
-  var counter = 0
-  var list = document.querySelectorAll('#grand-node > div')
-    for (var i = 0; i < list.length; i++) {
-      var current_child = list[i]
-    while (current_child.querySelector(':first-child') != null) {
-      counter += 1
-      current_child = current_child.querySelector(':first-child')
-      deepestChild = current_child
+  let parent = document.getElementById("grand-node");
+  let child = parent.children[0];
 
-    }
-    if (children[i] === undefined || children[i] < counter) {
-      children[i] = counter
-      answer = deepestChild
-    }
-  counter = 0
+  while (child) {
+    parent = child;
+    child = parent.children[0];
   }
-  return answer
+  return parent;
 }
